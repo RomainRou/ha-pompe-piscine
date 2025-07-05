f4ef2380f2725d5dd57c255a10d81860c65050a2
RomainRou <121958814+RomainRou@users.noreply.github.com>
2025-07-05 13:01:03 +0200

    Update __init__.py

--- custom_components/pompe_piscine/__init__.py
+++ custom_components/pompe_piscine/__init__.py
@@ -1,5 +1,9 @@
-"""Intégration personnalisée pour la pompe de piscine."""
-DOMAIN = "pompe_piscine"
+from .const import DOMAIN
+from .automation_logic import setup_automation
 
-async def async_setup(hass, config):
+async def async_setup_entry(hass, entry):
+    setup_automation(hass, entry.data)
+    return True
+
+async def async_unload_entry(hass, entry):
     return True
