1b0fce81b6b6f555e59f7ab6b3de9f641eaddea6
RomainRou <121958814+RomainRou@users.noreply.github.com>
2025-07-05 13:15:56 +0200

    Update config_flow.py

--- custom_components/pompe_piscine/config_flow.py
+++ custom_components/pompe_piscine/config_flow.py
@@ -1,5 +1,10 @@
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.helpers.selector import (
+    EntitySelector,
+    EntitySelectorConfig,
+    Selector,
+)
 from .const import *
 
 class PompePiscineConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -9,14 +14,15 @@ class PompePiscineConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             return self.async_create_entry(title="Pompe Piscine", data=user_input)
 
-        schema = vol.Schema({
-            vol.Required(CONF_POMPE): str,
-            vol.Required(CONF_TEMP_EAU): str,
-            vol.Required(CONF_TEMP_EXT): str,
-            vol.Required(CONF_METEO): str,
-            vol.Required(CONF_TEMPS_CYCLE): int,
-            vol.Required(CONF_TELEGRAM_USER): str,
-            vol.Required(CONF_SAISON): str,
-        })
-
-        return self.async_show_form(step_id="user", data_schema=schema)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_POMPE): EntitySelector(EntitySelectorConfig(domain="switch")),
+                vol.Required(CONF_TEMP_EAU): EntitySelector(EntitySelectorConfig(domain="sensor")),
+                vol.Required(CONF_TEMP_EXT): EntitySelector(EntitySelectorConfig(domain="sensor")),
+                vol.Required(CONF_METEO): EntitySelector(EntitySelectorConfig(domain="weather")),
+                vol.Required(CONF_TEMPS_CYCLE): vol.All(vol.Coerce(int), vol.Range(min=1)),
+                vol.Required(CONF_TELEGRAM_USER): str,  # Could be a text input or selector if Telegram integration exists
+                vol.Required(CONF_SAISON): vol.In(["été", "hiver", "automne", "printemps"]),  # or use selector
+            })
+        )
