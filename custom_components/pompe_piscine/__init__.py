0aa9c8df1506e02dcbf1f874c71ec9ec701220c1
RomainRou <121958814+RomainRou@users.noreply.github.com>
2025-07-05 16:14:20 +0200

    Update __init__.py

--- custom_components/pompe_piscine/__init__.py
+++ custom_components/pompe_piscine/__init__.py
@@ -1,3 +1,8 @@
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+
+DOMAIN = "pompe_piscine"
+
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(config_entry, "switch")
