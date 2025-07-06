cf710186cd9acc900a22681f63b8646032e281ab
RomainRou <121958814+RomainRou@users.noreply.github.com>
2025-07-06 10:10:36 +0200

    Update config_flow.py

--- custom_components/pompe_piscine/config_flow.py
+++ custom_components/pompe_piscine/config_flow.py
@@ -1,21 +1,43 @@
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.helpers.selector import (
+    EntitySelector,
+    EntitySelectorConfig,
+    TextSelector,
+    TextSelectorConfig
+)
 from . import const
-
-STEP_SCHEMA = vol.Schema({
-    vol.Required(const.CONF_POMPE_SWITCH): str,
-    vol.Required(const.CONF_SENSOR_TEMP): str,
-    vol.Required(const.CONF_SENSOR_TEMP_EXT): str,
-    vol.Required(const.CONF_SENSOR_SAISON): str,
-    vol.Required(const.CONF_WEATHER): str,
-    vol.Required(const.CONF_MODE_SELECT): str,
-    vol.Required(const.CONF_TELEGRAM_ID): str,
-})
 
 class ConfigFlow(config_entries.ConfigFlow, domain=const.DOMAIN):
     VERSION = 1
-    
+
     async def async_step_user(self, user_input=None):
-        if user_input is None:
-            return self.async_show_form(step_id="user", data_schema=STEP_SCHEMA)
-        return self.async_create_entry(title="Pompe Piscine", data=user_input)
+        if user_input is not None:
+            return self.async_create_entry(title="Pompe Piscine", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(const.CONF_POMPE_SWITCH): EntitySelector(
+                    EntitySelectorConfig(domain="switch")
+                ),
+                vol.Required(const.CONF_SENSOR_TEMP): EntitySelector(
+                    EntitySelectorConfig(domain="sensor", device_class="temperature")
+                ),
+                vol.Required(const.CONF_SENSOR_TEMP_EXT): EntitySelector(
+                    EntitySelectorConfig(domain="sensor", device_class="temperature")
+                ),
+                vol.Required(const.CONF_SENSOR_SAISON): EntitySelector(
+                    EntitySelectorConfig(domain="sensor")
+                ),
+                vol.Required(const.CONF_WEATHER): EntitySelector(
+                    EntitySelectorConfig(domain="weather")
+                ),
+                vol.Required(const.CONF_MODE_SELECT): EntitySelector(
+                    EntitySelectorConfig(domain="input_select")
+                ),
+                vol.Required(const.CONF_TELEGRAM_ID): TextSelector(
+                    TextSelectorConfig(type="text", multiline=False)
+                ),
+            })
+        )
