b1191a9620fe413531cd930a0d1e2912b30b4e61
RomainRou <121958814+RomainRou@users.noreply.github.com>
2025-07-06 08:50:13 +0200

    Update config_flow.py

--- custom_components/pompe_piscine/config_flow.py
+++ custom_components/pompe_piscine/config_flow.py
@@ -1,28 +1,20 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.helpers.selector import (
-    EntitySelector,
-    EntitySelectorConfig,
-    TextSelector,
-)
-from .const import *
+from homeassistant.core import callback
+from .const import DOMAIN
 
-class PompePiscineConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class PiscineConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="Pompe Piscine", data=user_input)
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Required(CONF_POMPE): EntitySelector(EntitySelectorConfig(domain="switch")),
-                vol.Required(CONF_TEMP_EAU): EntitySelector(EntitySelectorConfig(domain="sensor", device_class="temperature")),
-                vol.Required(CONF_TEMP_EXT): EntitySelector(EntitySelectorConfig(domain="sensor", device_class="temperature")),
-                vol.Required(CONF_METEO): EntitySelector(EntitySelectorConfig(domain="weather")),
-                vol.Required(CONF_SAISON): EntitySelector(EntitySelectorConfig(domain="sensor")), 
-                vol.Required(CONF_TEMPS_CYCLE): vol.All(vol.Coerce(int), vol.Range(min=1)),
-                vol.Required(CONF_TELEGRAM_USER): TextSelector(),
-            })
-        )
+            return self.async_create_entry(title="Piscine Intelligente", data=user_input)
+        schema = vol.Schema({
+            vol.Required("switch_pompe"): str,
+            vol.Required("sensor_temp_piscine"): str,
+            vol.Required("sensor_temp_exterieure"): str,
+            vol.Required("sensor_saison"): str,
+            vol.Required("weather_entity"): str,
+            vol.Required("telegram_user"): str
+        })
+        return self.async_show_form(step_id="user", data_schema=schema)
