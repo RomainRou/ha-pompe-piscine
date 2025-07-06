c79926dc8c979292ddedf65c59a195d7de0fda7b
RomainRou <121958814+RomainRou@users.noreply.github.com>
2025-07-06 09:43:00 +0200

    Update const.py

--- custom_components/pompe_piscine/const.py
+++ custom_components/pompe_piscine/const.py
@@ -1,2 +1,26 @@
 DOMAIN = "pompe_piscine"
-DEFAULT_NAME = "Pompe Piscine Intelligente"
+CONF_POMPE_SWITCH = "pompe_switch"
+CONF_SENSOR_TEMP = "sensor_temp"
+CONF_SENSOR_TEMP_EXT = "sensor_temp_ext"
+CONF_SENSOR_SAISON = "sensor_saison"
+CONF_WEATHER = "weather_entity"
+CONF_MODE_SELECT = "mode_select"
+CONF_TELEGRAM_ID = "telegram_user"
+
+PLATFORMS = ["switch", "sensor"]
+MODE_OPTIONS = [
+    "Filtration normale",
+    "Anti-algues préventif",
+    "Anti-algues curatif",
+    "Chlore lent",
+    "Chlore choc",
+    "Oxygène actif",
+    "Floculant",
+    "pH+ ou pH-",
+    "Algicide longue durée",
+    "Traitement multi-actions",
+    "Brome",
+    "Électrolyse au sel",
+    "Anti-calcaire préventif",
+    "Anti-calcaire curatif",
+]
