e957c401cad33cd7c6cebb9adb83722cfe319a96
RomainRou <121958814+RomainRou@users.noreply.github.com>
2025-07-06 09:45:13 +0200

    Update sensor.py

--- custom_components/pompe_piscine/sensor.py
+++ custom_components/pompe_piscine/sensor.py
@@ -1,84 +1,58 @@
-from datetime import datetime, timedelta
-from homeassistant.helpers.entity import Entity
-from homeassistant.const import STATE_ON, STATE_OFF
+from homeassistant.components.sensor import SensorEntity
+from . import const
 
-class PiscineSensorBase(Entity):
-    def __init__(self, hass):
-        self.hass = hass
-        self._attr = {}
-        self._state = None
+async def async_setup_entry(hass, entry, async_add_entities):
+    data = entry.data
+    async_add_entities([
+        CycleEnCoursSensor(), CycleNomSensor(),
+        DureeTotaleSensor(), TempsRestantSensor()
+    ])
 
-    async def async_update(self):
-        pass
-
-class CycleEnCoursSensor(PiscineSensorBase):
+class BaseSensor(SensorEntity):
     @property
-    def name(self):
-        return "cycle_en_cours_txt"
+    def available(self):
+        return True
 
-    @property
-    def state(self):
-        cycle_state = self.hass.states.get("input_boolean.cycle_en_cours")
-        return "Oui" if cycle_state and cycle_state.state == STATE_ON else "Non"
-
+class CycleEnCoursSensor(BaseSensor):
     @property
     def unique_id(self):
         return "piscine_cycle_en_cours"
-
-class DureeCycleTotal(PiscineSensorBase):
     @property
     def name(self):
-        return "duree_cycle_total"
-
+        return "Cycle en cours"
     @property
     def state(self):
-        duree = self.hass.states.get("input_number.duree_cycle")
-        if duree and duree.state not in (None, ""):
-            return round(float(duree.state) / 60)  # minutes
-        return 0
+        return self.hass.states.get("input_boolean.piscine_cycle_en_cours").state.title()
 
+class CycleNomSensor(BaseSensor):
     @property
-    def unit_of_measurement(self):
-        return "min"
+    def unique_id(self):
+        return "piscine_cycle_nom"
+    @property
+    def name(self):
+        return "Nom cycle"
+    @property
+    def state(self):
+        return self.hass.states.get("input_select.piscine_mode").state or "Aucun"
 
+class DureeTotaleSensor(BaseSensor):
     @property
     def unique_id(self):
         return "piscine_duree_cycle"
-
-class DureeCycleRestante(PiscineSensorBase):
     @property
     def name(self):
-        return "duree_cycle_restante"
-
+        return "Durée cycle (h:mm)"
     @property
     def state(self):
-        debut = self.hass.states.get("input_datetime.cycle_debut")
-        duree = self.hass.states.get("input_number.duree_cycle")
-        en_cours = self.hass.states.get("input_boolean.cycle_en_cours")
+        return self.hass.states.get("sensor._durée_total").state or "0:00"
 
-        if not (debut and duree and en_cours and en_cours.state == STATE_ON):
-            return 0
-
-        try:
-            debut_dt = datetime.strptime(debut.state, "%Y-%m-%d %H:%M:%S")
-            now = datetime.now()
-            fin = debut_dt + timedelta(seconds=int(float(duree.state)))
-            restante = (fin - now).total_seconds()
-            return max(0, round(restante / 60))
-        except Exception:
-            return 0
-
-    @property
-    def unit_of_measurement(self):
-        return "min"
-
+class TempsRestantSensor(BaseSensor):
     @property
     def unique_id(self):
-        return "piscine_duree_cycle_restante"
-
-async def async_setup_platform(hass, config, add_entities, discovery_info=None):
-    add_entities([
-        CycleEnCoursSensor(hass),
-        DureeCycleTotal(hass),
-        DureeCycleRestante(hass)
-    ])
+        return "piscine_temps_restant"
+    @property
+    def name(self):
+        return "Temps restant (h:mm)"
+    @property
+    def state(self):
+        return self.hass.states.get("sensor._temps_restant").state or "0:00"
