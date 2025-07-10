6d5ac4f16c47ad1d6e6238bb200db841dd81f6a6
RomainRou <121958814+RomainRou@users.noreply.github.com>
2025-07-10 15:55:05 +0200

    Update __init__.py

--- custom_components/pompe_piscine/__init__.py
+++ custom_components/pompe_piscine/__init__.py
@@ -1,23 +1,4 @@
-"""Init integration"""
-from homeassistant.core import HomeAssistant
-from homeassistant.config_entries import ConfigEntry
-from .const import DOMAIN
+DOMAIN = "piscine_manager"
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    return True
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
-
-    await hass.helpers.discovery.async_load_platform(entry, "switch")
-    await hass.helpers.discovery.async_load_platform(entry, "sensor")
-    
-    from .coordinator import PiscineCoordinator
-    coordinator = PiscineCoordinator(hass, entry)
-    hass.data[DOMAIN][entry.entry_id + "_coord"] = coordinator
-
-    return True
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    hass.data[DOMAIN].pop(entry.entry_id + "_coord", None)
+def setup(hass, config):
     return True
